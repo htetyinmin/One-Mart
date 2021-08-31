48e257f4d795a6b9e5043d842516babfbf672135
mgkyawmin <kyawmintun.kmt4142020@gmail.com>
2021-08-31 21:47:08 +0630

    change php extension

--- index.php
+++ index.php
@@ -1,24 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>POS</title>
-
-  <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" />
-
-  <!-- Fontawsome CSS -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
-
-  <!-- Style CSS -->
-  <link rel="stylesheet" href="./assets/frontend/css/customize.css">
-
-</head>
-<body>
 
   <!-- Navbar -->
+ <?php 
+    include_once "./template/header.php";
+ ?>
+ 
+ 
+ 
   <nav class="navbar navbar-expand-lg navbar-light nav-color">
     <div class="container">
       <a class="navbar-brand" href="index.html" title="home">
@@ -250,71 +237,10 @@
   </div>
 
 
-  <!-- footer -->
-  <footer class="pt-5 pb-2">
-    <div class="footer_int">
-      <div class="item">
-        <div class="title">COMPANY</div>
-        <p><a href="#">About Us</a></p>
-        <p><a href="#">Careers</a></p>
-        <p><a href="#">Contact</a></p>
-        <p><a href="#">Blog</a></p>
-        <p><a href="#">Sitemap</a></p>
-      </div>
-
-      <div class="item">
-        <div class="title">POLICY INFO</div>
-        <p><a href="#">Privacy Policy</a></p>
-        <p><a href="#">Terms of Sale</a></p>
-        <p><a href="#">Terms of Use</a></p>
-        <p><a href="#">Report Issue</a></p>
-      </div>
-
-      <div class="item">
-        <div class="title">CONTACT US</div>
-        <p><a href="#">No(123), 86-B, Yangon.</a></p>
-        <p><a href="#">exampleinfo@gmail.com</a></p>
-        <p><a href="#">exampleinfo.hr@gmail.com</a></p>
-        <p><a href="#">+(95) 123-123-123</a></p>
-        <p><a href="#">+(95) 123-456-789</a></p>
-      </div>
-
-      <div class="item">
-        <div class="title">NEED HELP ?</div>
-        <p><a href="#">FAQ</a></p>
-        <p><a href="#">Contact</a></p>
-        <p><a href="#">Online Shoping</a></p>
-        <p><a href="#">Report Issue</a></p>
-      </div>
-      <div class="item">
-        <div class="title">SUBSCRIBE</div>
-          <form>
-            <input type="email" name="email" id="" placeholder="your email address" required autocomplete="off">
-            <input type="submit" value="Subscribe">
-          </form>
-          <span>Register now to get updates on promotions and coupons.</span>
-      </div>
-    </div>
-    <div class="social_media">
-      <a href="#" title="facebook"><i class="fab fa-facebook-f"></i></a>
-      <a href="#" title="youtube"><i class="fab fa-youtube"></i></a>
-      <a href="#" title="whatsapp"><i class="fab fa-whatsapp"></i></a>
-      <a href="#" title="viber"><i class="fab fa-viber"></i></a>
-    </div>
-    <hr>
-
-    <!-- copyright -->
-    <div class="copyright">
-      &copy;&nbsp;2021-<script>var d = new Date();document.write(d.getFullYear());</script>. All right reserved.
-    </div>
-
-  </footer>
 
 
-  <!-- javascript code -->
-  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
-  <script src="./assets/frontend/js/app.js"></script>
-</body>
-</html>
+
+
+<?php 
+  include_once "./template/footer.php";
+?>
---
+++ template/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>POS</title>
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+
+  <!-- Fontawsome CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+
+  <!-- Style CSS -->
+  <link rel="stylesheet" href="./assets/frontend/css/customize.css">
+
+</head>
+<body>
