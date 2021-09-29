255f9703ed551f306dcb54fad91c20b691354b45
mgkyawmin <kyawmintun.kmt4142020@gmail.com>
2021-09-29 19:11:53 +0630

    change style css

--- sql/onemart.sql
+++ sql/onemart.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 02, 2021 at 08:50 AM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 8.0.3
+-- Generation Time: Sep 26, 2021 at 04:18 PM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,6 +35,13 @@ CREATE TABLE `brand` (
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `brand`
+--
+
+INSERT INTO `brand` (`id`, `name`, `photo`, `created_at`, `updated_at`) VALUES
+(12, 'Dell', '3262864552_dell.png', '2021-09-11 07:58:03', '2021-09-11 07:58:03');
+
 -- --------------------------------------------------------
 
 --
@@ -47,6 +54,14 @@ CREATE TABLE `categories` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'Test 3', '2021-09-04 06:12:09', '2021-09-04 06:12:09'),
+(59, 'V', '2021-09-25 14:14:54', '2021-09-25 14:14:54');
 
 -- --------------------------------------------------------
 
@@ -75,13 +90,23 @@ CREATE TABLE `items` (
   `name` varchar(255) NOT NULL,
   `photo` text NOT NULL,
   `price` varchar(255) NOT NULL,
-  `discount` varchar(225) NOT NULL,
+  `discount` varchar(225) DEFAULT NULL,
   `description` text NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `brand_id` int(11) NOT NULL,
   `subcategory_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`id`, `codeno`, `name`, `photo`, `price`, `discount`, `description`, `created_at`, `updated_at`, `brand_id`, `subcategory_id`) VALUES
+(9, 'SS-98765', 'iPhone 12 Pro', '3263020500_iphone-12-pro.jpg', '1200', '1099', '128GB', '2021-09-12 13:00:03', '2021-09-12 13:00:03', 12, 8),
+(13, 'SS-98765', 'iPhone 13 Pro', '3263020500_iphone-12-pro.jpg', '1200', '1099', '128GB', '2021-09-12 13:00:03', '2021-09-12 13:00:03', 12, 8),
+(14, 'SS-98765', 'iPhone 14 Pro', '3263020500_iphone-12-pro.jpg', '1200', '1099', '128GB', '2021-09-12 13:00:03', '2021-09-12 13:00:03', 12, 8),
+(15, 'SS-98765', 'iPhone 15 Pro', '3263020500_iphone-12-pro.jpg', '1200', '1099', '128GB', '2021-09-12 13:00:03', '2021-09-12 13:00:03', 12, 8);
 
 -- --------------------------------------------------------
 
@@ -109,6 +134,13 @@ CREATE TABLE `model_has_role` (
   `user_id` int(11) NOT NULL,
   `role_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `model_has_role`
+--
+
+INSERT INTO `model_has_role` (`id`, `user_id`, `role_id`) VALUES
+(1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -140,6 +172,14 @@ CREATE TABLE `role` (
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'Admin', '2021-09-12 14:18:56', '2021-09-12 14:18:56'),
+(2, 'User', '2021-09-12 14:18:56', '2021-09-12 14:18:56');
+
 -- --------------------------------------------------------
 
 --
@@ -149,10 +189,19 @@ CREATE TABLE `role` (
 CREATE TABLE `subcategories` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
+  `photo` text NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `category_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `subcategories`
+--
+
+INSERT INTO `subcategories` (`id`, `name`, `photo`, `created_at`, `updated_at`, `category_id`) VALUES
+(8, 'Phones', '3262860470_Beat.jpg', '2021-09-11 07:57:45', '2021-09-11 07:57:45', 1),
+(14, 'aa', '3265261948_amazfit neo.jpg', '2021-09-26 04:36:14', '2021-09-26 04:36:14', 1);
 
 -- --------------------------------------------------------
 
@@ -169,6 +218,13 @@ CREATE TABLE `users` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `phone`, `created_at`, `updated_at`) VALUES
+(1, 'JackParker', 'admin@gmail.com', 'd2.WPbATM58vQ', '09876543210', '2021-09-12 14:20:39', '2021-09-12 14:20:39');
 
 --
 -- Indexes for dumped tables
@@ -198,8 +254,8 @@ ALTER TABLE `cupons`
 --
 ALTER TABLE `items`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `brand_id` (`brand_id`),
-  ADD UNIQUE KEY `subcategory_id` (`subcategory_id`);
+  ADD KEY `fk` (`brand_id`),
+  ADD KEY `fkk` (`subcategory_id`);
 
 --
 -- Indexes for table `item_order`
@@ -214,8 +270,8 @@ ALTER TABLE `item_order`
 --
 ALTER TABLE `model_has_role`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `user_id` (`user_id`),
-  ADD UNIQUE KEY `role_id` (`role_id`);
+  ADD KEY `user_fk` (`user_id`),
+  ADD KEY `role_fk` (`role_id`);
 
 --
 -- Indexes for table `orders`
@@ -251,13 +307,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `brand`
 --
 ALTER TABLE `brand`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
 
 --
 -- AUTO_INCREMENT for table `cupons`
@@ -269,7 +325,7 @@ ALTER TABLE `cupons`
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `item_order`
@@ -281,7 +337,7 @@ ALTER TABLE `item_order`
 -- AUTO_INCREMENT for table `model_has_role`
 --
 ALTER TABLE `model_has_role`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `orders`
@@ -293,23 +349,36 @@ ALTER TABLE `orders`
 -- AUTO_INCREMENT for table `role`
 --
 ALTER TABLE `role`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `subcategories`
 --
 ALTER TABLE `subcategories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `items`
+--
+ALTER TABLE `items`
+  ADD CONSTRAINT `fkk` FOREIGN KEY (`subcategory_id`) REFERENCES `subcategories` (`id`);
+
+--
+-- Constraints for table `model_has_role`
+--
+ALTER TABLE `model_has_role`
+  ADD CONSTRAINT `role_fk` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`),
+  ADD CONSTRAINT `user_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `subcategories`
