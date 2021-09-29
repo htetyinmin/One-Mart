f4245daa55597a9c213f178a89073d9213aeb1a7
htetyinmin <htetyinmin9888@gmail.com>
2021-09-29 11:33:34 +0630

    contact complete

--- contact.php
+++ contact.php
@@ -6,71 +6,47 @@
 
     <section id="contact">
         <div class="container">
-            <form>
-                <div class="row mb-4">
-                    <div class="col">
-                    <div class="form-outline">
-                        <input type="text" id="form3Example1" class="form-control" />
-                        <label class="form-label" for="form3Example1">First name</label>
+            <div class="contact-header text-center">
+                <h2>Contact Us</h2>
+            </div>
+            <div class="contact-form">
+                <form>
+                    <div class="row">
+                        <div class="col-6">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30609.5133163216!2d97.64416685575493!3d16.46595654763528!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30c2a8bdc423043f%3A0x77b6f712c8e07d17!2sOcean%20Supercenter%20(Mawlamyine)!5e0!3m2!1sen!2ssg!4v1632891325484!5m2!1sen!2ssg" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                        </div>
+
+                        <div class="col-6">
+                            <!-- Name input -->
+                            <div class="form-outline mb-2">
+                                <input type="text" id="name" class="form-control" required />
+                                <label class="form-label" for="name">Name</label>
+                            </div>
+
+                            <!-- Phone input -->
+                            <div class="form-outline mb-2">
+                                <input type="text" id="phone" class="form-control" required />
+                                <label class="form-label" for="phone">Phone Number</label>
+                            </div>
+        
+                            <!-- Email input -->
+                            <div class="form-outline mb-2">
+                                <input type="email" id="email" class="form-control" required />
+                                <label class="form-label" for="email">Email address</label>
+                            </div>
+        
+                            <!-- Message input -->
+                            <div class="form-outline mb-2">
+                                <textarea class="form-control" id="message" rows="4" required></textarea>
+                                <label class="form-label" for="message">Message</label>
+                            </div>
+        
+                            <!-- Submit button -->
+                            <button type="submit" class="btn btn-primary btn-block mb-2">Send</button>
+                        </div>
                     </div>
-                    </div>
-                    <div class="col">
-                    <div class="form-outline">
-                        <input type="text" id="form3Example2" class="form-control" />
-                        <label class="form-label" for="form3Example2">Last name</label>
-                    </div>
-                    </div>
-                </div>
-    
-                <!-- Email input -->
-                <div class="form-outline mb-4">
-                    <input type="email" id="form3Example3" class="form-control" />
-                    <label class="form-label" for="form3Example3">Email address</label>
-                </div>
-    
-                <!-- Password input -->
-                <div class="form-outline mb-4">
-                    <input type="password" id="form3Example4" class="form-control" />
-                    <label class="form-label" for="form3Example4">Password</label>
-                </div>
-    
-                <!-- Checkbox -->
-                <div class="form-check d-flex justify-content-center mb-4">
-                    <input
-                    class="form-check-input me-2"
-                    type="checkbox"
-                    value=""
-                    id="form2Example3"
-                    checked
-                    />
-                    <label class="form-check-label" for="form2Example3">
-                    Subscribe to our newsletter
-                    </label>
-                </div>
-    
-                <!-- Submit button -->
-                <button type="submit" class="btn btn-primary btn-block mb-4">Sign up</button>
-    
-                <!-- Register buttons -->
-                <div class="text-center">
-                    <p>or sign up with:</p>
-                    <button type="button" class="btn btn-primary btn-floating mx-1">
-                    <i class="fab fa-facebook-f"></i>
-                    </button>
-    
-                    <button type="button" class="btn btn-primary btn-floating mx-1">
-                    <i class="fab fa-google"></i>
-                    </button>
-    
-                    <button type="button" class="btn btn-primary btn-floating mx-1">
-                    <i class="fab fa-twitter"></i>
-                    </button>
-    
-                    <button type="button" class="btn btn-primary btn-floating mx-1">
-                    <i class="fab fa-github"></i>
-                    </button>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
     </section>
 
